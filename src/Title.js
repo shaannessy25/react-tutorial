a7a76084f6378df61726a80b453594cd02058b5f
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-04-23 10:13:58 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+        <div className='Title'> 
+            <header>
+                <h1>First React App</h1>
+            </header>
+            <div className='Title-Subtitle'> 
+                <header>
+                    <h1>Created by: Shaan Hurley</h1>
+                </header>
+             </div>
+        </div>
+
+        
+    )
+}
+
+export default Title
